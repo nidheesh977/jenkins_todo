c4fd11e07f72497bfc7ecb77dacc11f2d4cd00dd
Nivin1122 <bnivin71@gmail.com>
2025-09-30 16:23:27 +0530

    feat: clear tasks

--- task.py
+++ task.py
@@ -5,11 +5,18 @@ def print_tasks(tasks):
         print(task)
 
 def create_task():
+    global tasks
     tasks.append(input("Enter new task"))
     print_tasks(tasks)
 
 def delete_task(index):
+    global tasks
     tasks.pop(index)
     print_tasks(tasks)
+
+def delete_all():
+    global tasks
+    tasks = []
+
 
 create_task()
